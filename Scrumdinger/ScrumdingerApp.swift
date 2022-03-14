afb85aa847ef93e5b98b88b7a67780b289cf7711
Jarcok7 <Jarcok7@gmail.com>
2022-03-14 12:00:03 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yaroslav Katsalay on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
